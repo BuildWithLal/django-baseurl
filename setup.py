b87150dbc0962590d350f1d573cbee89898e90f1
Lal Zada <lalzadamohmand@gmail.com>
2017-02-03 12:25:41 +0500

    Prepared for PyPi and separate README.rst

--- setup.py
+++ setup.py
@@ -1,35 +1,38 @@
 import os
 from setuptools import find_packages, setup
 
-with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
+with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
     README = readme.read()
+
 
 # allow setup.py to be run from any path
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django_baseurl',
-    version='1.0',
+    version='1.01',
     packages=find_packages(),
     include_package_data=True,
-    license='BSD License',  # example license
+    license='GNU General Public License (GPL)',
     description='A Django app which return a BASE_URL template context for the current request.',
     long_description=README,
-    url='https://www.sparkanswers.com/',
+    url='https://github.com/lalzada/django-baseurl.git',
     author='Lal Zada',
     author_email='lalzadamohmand@gmail.com',
     classifiers=[
         'Environment :: Web Environment',
         'Framework :: Django',
-        'Framework :: Django :: 1.10',  # replace "X.Y" as appropriate
         'Intended Audience :: Developers',
-        'License :: GNU General Public License',  # example license
+        'License :: OSI Approved :: GNU General Public License (GPL)',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         # Replace these appropriately if you are stuck on Python 2.
         'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Topic :: Internet :: WWW/HTTP',
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
